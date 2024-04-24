1eda357c1ce5c13c3b4f620e5dd38099e1bffbf9
zaldih <imzaldih@gmail.com>
2024-04-24 18:35:08 +0200

    chore: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site : 'https://voidcosmos.github.io',
-  base : 'npkill-web',
+  site: "https://npkill.js.org",
 });
